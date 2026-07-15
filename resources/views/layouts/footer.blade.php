2535bbd3e303c40b58041ac24c5e6708100e7fc5
andreryn <andreryanbaskoro@gmail.com>
2026-07-15 16:51:51 +0900

    UI Fix: Change footer brand text to ARB

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="container-fluid px-4 py-3">
         <div class="row align-items-center g-2">
             <div class="col-md-6 text-center text-md-start">
-                <span class="fw-semibold text-brand">Cahaya Mulya Mart</span>
+                <span class="fw-semibold text-brand">ARB</span>
                 <span class="text-muted small ms-1">© {{ date('Y') }}</span>
             </div>
             <div class="col-md-6 text-center text-md-end">
